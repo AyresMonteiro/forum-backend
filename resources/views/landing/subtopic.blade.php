4f3c9efafd19a19065739c11b82fcbb39463c2c1
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-03-09 22:56:34 -0300

    [Add] moldes das views da landing page

---
+++ resources/views/landing/subtopic.blade.php
@@ -0,0 +1,11 @@
+<div class="subtopic">
+  <div class="title">
+    Título do subtópico
+  </div>
+  <div class="summary">
+    Resumo do subtópico
+  </div>
+  <div class="last-post">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda molestias obcaecati ad reiciendis repellat possimus sapiente velit neque, fugiat nihil consectetur, itaque animi facere omnis cumque. Blanditiis rem labore aliquam.
+  </div>
+</div>
---
+++ resources/views/landing/topic.blade.php
@@ -0,0 +1,6 @@
+<div class="topic">
+  <div class="title">Tópico 1</div>
+  <div class="subtopics-container">
+    @include('landing.subtopic')
+  </div>
+</div>
